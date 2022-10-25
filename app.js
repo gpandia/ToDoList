cd39b1cda3838b2de01465f6e7eafaa90afa121a
Gokul Pandian C <gokulcofficial@gmail.com>
2022-10-25 11:59:27 +0530

    Add procfile, ports, gitignore

--- app.js
+++ app.js
@@ -132,6 +132,11 @@ app.post("/delete", function(req, res){
 //   res.render("about");
 // });
 
-app.listen(4000, function() {
-  console.log("Server started on port 4000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 4000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
