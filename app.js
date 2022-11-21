d4a80ccbaf6c202ac181fadb991881058b131d00
Gokul Pandian C <gokulcofficial@gmail.com>
2022-11-21 12:36:34 +0530

    fchange

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.CONNECT, {useNewUrlParser: true});
+mongoose.connect("process.env.CONNECT", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
