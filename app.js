3f8e09d3876470c07fb50d1ed76b3584c67318ad
Gokul Pandian C <gokulcofficial@gmail.com>
2022-11-21 12:55:45 +0530

    small c

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-gokul:${process.env.CONNECT}@cluster0.nqdhlkp.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-gokul:"+process.env.CONNECT+"@cluster0.nqdhlkp.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
