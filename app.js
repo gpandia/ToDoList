90818ef240e455bc70386037f2595865369c0557
Gokul Pandian C <gokulcofficial@gmail.com>
2022-11-21 12:47:49 +0530

    changes in env

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("process.env.CONNECT", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-gokul:${process.env.CONNECT}@cluster0.nqdhlkp.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
