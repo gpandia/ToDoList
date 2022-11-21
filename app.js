e5d775a543962a0e558ef788058bfd667c686fd4
Gokul Pandian C <gokulcofficial@gmail.com>
2022-11-21 12:31:18 +0530

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-gokul:Test%40123@cluster0.nqdhlkp.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect(process.env.CONNECT, {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
